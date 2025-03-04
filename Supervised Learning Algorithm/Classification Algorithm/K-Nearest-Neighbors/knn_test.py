e6ef3e2cf5f81c1ea6f39e2f5722ea3492e79bd2
Paras kumar <paraskumar5642@gmail.com>
2025-03-04 22:35:36 +0530

    K-NN update

---
+++ Supervised Learning Algorithm/Classification Algorithm/K-Nearest-Neighbors/knn_test.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from KNearestNeighbor import KNearestNeighbors
+
+data=pd.read_csv("C:\\Users\\Paras\\Desktop\\l\\K-Nearest-Neighbors\\Social_Network_Ads.csv")
+
+X=data.iloc[:,2:4].values
+y=data.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+
+# An object of KNN
+knn=KNearestNeighbors(k=3)
+
+knn.fit(X_train,y_train)
+
+def predict_new():
+    age=int(input("Enter the age"))
+    salary=int(input("Enter the salary"))
+    X_new=np.array([[age],[salary]]).reshape(1,2)
+
+    X_new=scaler.transform(X_new)
+    print(knn.predict(X_new))
+    result=knn.predict(X_new)
+    print(result)
+
+    if result==0:
+        print("Will not purchase")
+    else:
+        print("Will purchase")
+
+predict_new()
